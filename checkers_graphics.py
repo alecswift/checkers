08b7f58934673657ec2b75c1118b270a25389fb6
Alec Swift <aleceswift@gmail.com>
2023-03-12 00:42:27 -0800

    Move board image class to top of file

--- checkers_graphics.py
+++ checkers_graphics.py
@@ -13,6 +13,25 @@ player_1 = Player("black", board)
 player_2 = Player("white", board)
 
 
+class BoardImage(Board):
+    def __init__(self, surface, checkers):
+        super().__init__()
+        self._screen = pygame.display.set_mode((484, 484))
+        self._checkers = checkers  # group of checkers sprites
+        self._surface = surface
+
+    def display_board(self, screen):
+        for x_coord, y_coord in product(range(0, 364, 121), range(0, 364, 121)):
+            screen.blit(board_surface, (x_coord, y_coord))
+
+    def set_checkers(self):
+        for piece in self.board.values():
+            if piece in (None, "empty"):
+                continue
+            else:
+                checkers.add(CheckerSprite(piece))
+
+
 class CheckerSprite(pygame.sprite.Sprite):
     """moves a clenched fist on the screen, following the mouse"""
 
@@ -22,7 +41,7 @@ class CheckerSprite(pygame.sprite.Sprite):
     def __init__(self, piece):
         super().__init__()
         self._piece = piece
-        image_path = f'graphics/{piece.color}_piece.png'
+        image_path = f"graphics/{piece.color}_piece.png"
         self.image = pygame.image.load(image_path).convert_alpha()
         x_coord, y_coord = int(piece.pos.real), int(piece.pos.imag)
         screen_pos = (x_coord * self.COORD_FACTOR, y_coord * self.COORD_FACTOR)
@@ -36,30 +55,11 @@ class CheckerSprite(pygame.sprite.Sprite):
         pos = pygame.mouse.get_pos()
         self.rect.center = pos
 
-class BoardImage(Board):
-
-    def __init__(self, surface, checkers):
-        super().__init__()
-        self._screen = pygame.display.set_mode((484, 484))
-        self._checkers = checkers # group of checkers sprites
-        self._surface = surface
-
-    def display_board(self, screen):
-        for x_coord, y_coord in product(range(0, 364, 121), range(0, 364, 121)):
-            screen.blit(board_surface, (x_coord, y_coord))
-        
-    def set_checkers(self):
-        for piece in self.board.values():
-            if piece in (None, "empty"):
-                continue
-            else:
-                checkers.add(CheckerSprite(piece))
-
 
 board_surface = pygame.image.load("graphics/chessboard2.png").convert_alpha()
 checkers = pygame.sprite.Group()
 board_image = BoardImage(board_surface, checkers)
-board_image.set_checkers() # set outside loop for initial state
+board_image.set_checkers()  # set outside loop for initial state
 current_checker = None
 # correlate piece position tuples with piece position multiply x and y by 60.5
 # 6,5 -> 6*60.5, 5*60.5
@@ -72,7 +72,7 @@ while True:
         if event.type == pygame.MOUSEBUTTONUP:
             current_checker = None
             # go back to initial position
-    
+
     # Draw elements
     # screen.blit(checkers, (0,0), (0, 60, 60, 60)) # black man
     # screen.blit(checkers, (6*60.5, 5*60.5), (0, 0, 60, 60)) # black king
@@ -96,7 +96,7 @@ while True:
             if checker.rect.collidepoint(mouse_pos) and mouse_pressed:
                 checker.update()
                 current_checker = checker
-            
+
     # possibly rect collisions for landing on specific squares?
     # pygame.mouse position collides with a players piece plus press to pick up piece
 
@@ -106,5 +106,5 @@ while True:
     pygame.display.update()
     # clock.tick(60) # controls while loop hits/second
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
