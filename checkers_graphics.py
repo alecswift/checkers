1df732e8de6b89541381aac62f4f32e1bc602832
Alec Swift <aleceswift@gmail.com>
2023-03-11 18:19:06 -0800

    Add crops for each checkers piece for the checkers surface

--- checkers_graphics.py
+++ checkers_graphics.py
@@ -9,7 +9,8 @@ clock = pygame.time.Clock()
 
 board_surface = pygame.image.load('graphics/chessboard2.png').convert_alpha()
 checkers = pygame.image.load('graphics/checker.png').convert_alpha()
-
+# correlate piece position tuples with piece position multiply x and y by 60.5
+# 6,5 -> 6*60.5, 5*60.5
 
 while True:
     for event in pygame.event.get():
@@ -18,8 +19,12 @@ while True:
             exit()
     for x_coord, y_coord in product(range(0, 364, 121), range(0, 364, 121)):
         screen.blit(board_surface, (x_coord, y_coord))
-    screen.blit(checkers, (0, 0), (0, 0, 60, 60)) # black_king
-    
+    screen.blit(checkers, (0,0), (0, 60, 60, 60)) # black man
+    screen.blit(checkers, (6*60.5, 5*60.5), (0, 0, 60, 60)) # black king
+    screen.blit(checkers, (60.5,60.5), (60, 60, 60, 60)) # white man
+    screen.blit(checkers, (60.5,0), (60, 0, 60, 60)) # white king
+    # possibly rect collisions for landing on specific squares?
+    # pygame.mouse position collides with a players piece plus press to pick up piece
     # Draw elements
     # update everything
     # move sprite from one square to another square
