405b7e6b592daba4c6b3e2d9b27402e5cdf06ef0
Alec Swift <aleceswift@gmail.com>
2023-03-11 21:13:34 -0800

    Add graphics for initial board state

--- checkers_graphics.py
+++ checkers_graphics.py
@@ -1,11 +1,17 @@
 import pygame
 from sys import exit
 from itertools import product
+from checkers import Checkers, Player
 
 pygame.init()
 screen = pygame.display.set_mode((484, 484))
 pygame.display.set_caption("Checkers")
 clock = pygame.time.Clock()
+
+game = Checkers()
+player_1 = Player("black", game)
+player_2 = Player("white", game)
+
 
 board_surface = pygame.image.load('graphics/chessboard2.png').convert_alpha()
 checkers = pygame.image.load('graphics/checker.png').convert_alpha()
@@ -19,10 +25,16 @@ while True:
             exit()
     for x_coord, y_coord in product(range(0, 364, 121), range(0, 364, 121)):
         screen.blit(board_surface, (x_coord, y_coord))
-    screen.blit(checkers, (0,0), (0, 60, 60, 60)) # black man
-    screen.blit(checkers, (6*60.5, 5*60.5), (0, 0, 60, 60)) # black king
-    screen.blit(checkers, (60.5,60.5), (60, 60, 60, 60)) # white man
-    screen.blit(checkers, (60.5,0), (60, 0, 60, 60)) # white king
+    for pos, val in game.board.items():
+        if val not in (None, 'empty'):
+            if val.color == 'black':
+                screen.blit(checkers, (pos.real * 60.5, pos.imag * 60.5), (0, 60, 60, 60))
+            else:
+                screen.blit(checkers, (pos.real * 60.5, pos.imag * 60.5), (60, 60, 60, 60))
+    # screen.blit(checkers, (0,0), (0, 60, 60, 60)) # black man
+    # screen.blit(checkers, (6*60.5, 5*60.5), (0, 0, 60, 60)) # black king
+    # screen.blit(checkers, (60.5,60.5), (60, 60, 60, 60)) # white man
+    # screen.blit(checkers, (60.5,0), (60, 0, 60, 60)) # white king
     # possibly rect collisions for landing on specific squares?
     # pygame.mouse position collides with a players piece plus press to pick up piece
     # Draw elements
