f1aa853ef9f9399f7f243cef35218ac2e7106453
Alec Swift <aleceswift@gmail.com>
2023-03-12 00:41:18 -0800

    Add class BoardImage

--- checkers_graphics.py
+++ checkers_graphics.py
@@ -30,39 +30,73 @@ class CheckerSprite(pygame.sprite.Sprite):
 
     # https://stackoverflow.com/questions/16825645/how-to-make-a-sprite-follow-your-mouse-cursor-using-pygame
     def update(self):
-        "move the fist based on the mouse position"
+        "move the checker based on the mouse position"
+        # board.board[self._piece.pos] = "empty" # change so it only does this once or impossible?
+        # add layering over other checkers
         pos = pygame.mouse.get_pos()
         self.rect.center = pos
 
+class BoardImage(Board):
+
+    def __init__(self, surface, checkers):
+        super().__init__()
+        self._screen = pygame.display.set_mode((484, 484))
+        self._checkers = checkers # group of checkers sprites
+        self._surface = surface
+
+    def display_board(self, screen):
+        for x_coord, y_coord in product(range(0, 364, 121), range(0, 364, 121)):
+            screen.blit(board_surface, (x_coord, y_coord))
+        
+    def set_checkers(self):
+        for piece in self.board.values():
+            if piece in (None, "empty"):
+                continue
+            else:
+                checkers.add(CheckerSprite(piece))
+
 
 board_surface = pygame.image.load("graphics/chessboard2.png").convert_alpha()
+checkers = pygame.sprite.Group()
+board_image = BoardImage(board_surface, checkers)
+board_image.set_checkers() # set outside loop for initial state
+current_checker = None
 # correlate piece position tuples with piece position multiply x and y by 60.5
 # 6,5 -> 6*60.5, 5*60.5
-checkers = pygame.sprite.Group()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            current_checker = None
+            # go back to initial position
+    
     # Draw elements
     # screen.blit(checkers, (0,0), (0, 60, 60, 60)) # black man
     # screen.blit(checkers, (6*60.5, 5*60.5), (0, 0, 60, 60)) # black king
     # screen.blit(checkers, (60.5,60.5), (60, 60, 60, 60)) # white man
     # screen.blit(checkers, (60.5,0), (60, 0, 60, 60)) # white king
-    for x_coord, y_coord in product(range(0, 364, 121), range(0, 364, 121)):
-        screen.blit(board_surface, (x_coord, y_coord))
-    for pos, piece in board.board.items():
-        if piece in (None, "empty"):
-            continue
-        # maybe have black and white in seperate groups for interactions (capturing)?
-        else:
-            checker = CheckerSprite(piece)
-            checkers.add(CheckerSprite(piece))
+    # set this state outside of game loop, save the state and used the saved
+    # state rather than the board class everytime until a move has happened
+    # at which point update the state
+    board_image.display_board(screen)
     checkers.draw(screen)
 
     # User input
-
+    mouse_pos = pygame.mouse.get_pos()
+    if current_checker is not None:
+        mouse_pressed, *_ = pygame.mouse.get_pressed()
+        if current_checker.rect.collidepoint(mouse_pos) and mouse_pressed:
+            current_checker.update()
+    else:
+        for checker in checkers:
+            mouse_pressed, *_ = pygame.mouse.get_pressed()
+            if checker.rect.collidepoint(mouse_pos) and mouse_pressed:
+                checker.update()
+                current_checker = checker
+            
     # possibly rect collisions for landing on specific squares?
     # pygame.mouse position collides with a players piece plus press to pick up piece
 
@@ -71,3 +105,6 @@ while True:
     # correlate dictionary of pieces to the display
     pygame.display.update()
     # clock.tick(60) # controls while loop hits/second
+
+if __name__ == '__main__':
+    main()
