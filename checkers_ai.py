075672e69a1ea6f99809f2920ac0411cf419cbdd
Alec Swift <aleceswift@gmail.com>
2023-03-17 16:10:44 -0700

    Add valid moves function and test

--- checkers_ai.py
+++ checkers_ai.py
@@ -1,22 +1,34 @@
 from board import Piece, BoardDict
 from player import Path, Direction
 
-State = tuple[tuple[complex, int]]
+BoardState = tuple[tuple[complex, int]]
 
-def init_state(board: BoardDict) -> State:
+def init_state(board: BoardDict) -> BoardState:
     state = []
-    for val in board.values():
+    for pos, val in board.items():
         if isinstance(val, Piece):
             if val.color == "black":
                 color = 1
             else:
-                color = 0
+                color = 2
             if val.rank == "king":
-                color += 1
+                color += 2
             state.append((val.pos, color))
+        elif val == "empty":
+            state.append((pos, -0))
     return tuple(state)
 
-def minimax(state: State, depth: int, max_player: bool) -> Path:
+def init_borders():
+    """Return a set of border coordinates for a checkers board"""
+    borders = set()
+    borders_nums = [-1, 8]
+    for num1 in borders_nums:
+        for num2 in range(0,7):
+            borders.add(complex(num1, num2))
+            borders.add(complex(num2, num1))
+    return borders
+
+def minimax(state, depth: int, max_player: bool) -> Path:
     if depth == 0 or game_won(state):
         return evaluate(state) # return move as well?
     
@@ -34,19 +46,83 @@ def minimax(state: State, depth: int, max_player: bool) -> Path:
             curr_eval = minimax(state, depth - 1, True)
             min_eval = min(max_eval, curr_eval)
         return min_eval
+    
+class State:
 
-def valid_moves(state: State, max_player) -> list[Path]:
-    color = 0 if max_player else 1
-    paths: list[Path] = []
-    for pos, piece in state:
-        if piece % 2 == color:
-            path = (pos, )
-            capture = False
-            next_move(path, paths, piece, capture)
-    return paths
+    def __init__(self, board_state, borders):
+        self._board_state = board_state
+        self._borders = borders
+        self._paths = []
 
-def next_move(path: Path, paths: list[Path], piece: int, capture: bool) -> None;
-    pass
+    @property
+    def paths(self):
+        return self._paths
+
+    def valid_moves(self, max_player) -> list[Path]:
+        color = 0 if max_player else 1
+        for pos, piece in self._board_state:
+            if piece % 2 == color:
+                path = (pos, )
+                capture = False
+                self.next_move(path, piece, capture)
+        self.prune_paths()
+
+    def next_move(self, path: Path, piece: int, capture: bool) -> None:
+        if not capture:
+            prev_pos = None
+            *_, curr_pos = path
+        else:
+            *_, prev_pos, curr_pos = path
+        forward_left = Direction.FORWARD_LEFT.move(curr_pos, piece % 2, 1)
+        forward_right = Direction.FORWARD_RIGHT.move(curr_pos, piece % 2, 1)
+        next_positions = [forward_left, forward_right]
+        if piece <= 2:
+            for next_position in next_positions:
+                next_path = *path, next_position
+                self.build_path(next_path, piece, capture)
+        else:
+            back_left = Direction.BACK_LEFT.move(curr_pos, piece % 2, 1)
+            back_right = Direction.BACK_RIGHT.move(curr_pos, piece % 2, 1)
+            next_positions.extend([back_left, back_right])
+            for next_position in next_positions:
+                if prev_pos == next_position:
+                    continue
+                next_path = *path, next_position
+                self.build_path(next_path, piece % 2, capture)
+
+    def build_path(self, path: Path, piece: int, capture: bool) -> None:
+        *rest, curr_pos, next_pos = path
+        for pos, val in self._board_state:
+            if pos == next_pos:
+                next_val = val
+                break
+        else:
+            next_val = None
+
+        opp_color = 1 if piece % 2 == 0 else 0
+        if not capture and next_val == 0:
+            self._paths.append(path)
+        elif capture and not next_val:
+            self._paths.append((*rest, curr_pos))
+        elif capture and next_val and next_val % 2 != opp_color:
+            self._paths.append((*rest, curr_pos))
+        elif next_val and next_val % 2 == opp_color:
+            move = next_pos - curr_pos
+            jump = next_pos + move
+            if self.board.get(jump) == "empty":
+                self.next_move((*path, jump), piece, True)
+            elif capture:
+                self._paths.append((*rest, curr_pos))
+
+    def prune_paths(self) -> list[Path]:
+        """
+        Removes all invalid paths from the given paths list. A path is invalid if
+        there is another path in the list with more captures
+        """
+        if self._paths:
+            max_length = max(len(path) for path in self._paths)
+            return [path for path in self._paths if len(path) == max_length]
+        return self._paths
 
 def make_move(move: Path, state: State):
     pass
--- checkers_ai_test.py
+++ checkers_ai_test.py
@@ -5,23 +5,31 @@ import checkers_ai
 
 class TestCheckersAi(unittest.TestCase):
 
+    def setUp(self):
+        self.board = Board()
+        self.initial_board_dict = self.board.board
+        self.initial_state = checkers_ai.init_state(self.initial_board_dict)
+
+    def tearDown(self):
+        del self.board
+        del self.initial_board_dict
+        del self.initial_state
+
     def test_init_state_method_with_initial_board(self):
-        board = Board()
-        initial_board_dict = board.board
-        actual_output = set(checkers_ai.init_state(initial_board_dict))
+        actual_output = set(self.initial_state)
         expected = set((
-            (1+0j, 0,),
-            (3+0j, 0,),
-            (5+0j, 0,),
-            (7+0j, 0,),
-            (0+1j, 0,),
-            (2+1j, 0,),
-            (4+1j, 0,),
-            (6+1j, 0,),
-            (1+2j, 0,),
-            (3+2j, 0,),
-            (5+2j, 0,),
-            (7+2j, 0,),
+            (1+0j, 2,),
+            (3+0j, 2,),
+            (5+0j, 2,),
+            (7+0j, 2,),
+            (0+1j, 2,),
+            (2+1j, 2,),
+            (4+1j, 2,),
+            (6+1j, 2,),
+            (1+2j, 2,),
+            (3+2j, 2,),
+            (5+2j, 2,),
+            (7+2j, 2,),
             (0+5j, 1,),
             (2+5j, 1,),
             (4+5j, 1,),
@@ -34,16 +42,34 @@ class TestCheckersAi(unittest.TestCase):
             (2+7j, 1,),
             (4+7j, 1,),
             (6+7j, 1,),
-            (0+3j, None,),
-            (2+3j, None,),
-            (4+3j, None,),
-            (6+3j, None,),
-            (1+4j, None,),
-            (3+4j, None,),
-            (5+4j, None,),
-            (7+4j, None,),
+            (0+3j, 0,),
+            (2+3j, 0,),
+            (4+3j, 0,),
+            (6+3j, 0,),
+            (1+4j, 0,),
+            (3+4j, 0,),
+            (5+4j, 0,),
+            (7+4j, 0,),
         ))
         self.assertEqual(actual_output, expected)
+
+    def test_valid_move_method_with_initial_board_state(self):
+        borders = checkers_ai.init_borders()
+        state = checkers_ai.State(self.initial_state, borders)
+        state.valid_moves(False)
+        expected = set(
+            [
+                (5j, 1 + 4j),
+                (2 + 5j, 1 + 4j),
+                (2 + 5j, 3 + 4j),
+                (4 + 5j, 3 + 4j),
+                (4 + 5j, 5 + 4j),
+                (6 + 5j, 5 + 4j),
+                (6 + 5j, 7 + 4j),
+            ]
+        )
+        actual_output = set(state.paths)
+        self.assertEqual(expected, actual_output)
 
 if __name__ == "__main__":
     unittest.main()
