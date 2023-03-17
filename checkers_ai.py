f9c73c714a29fa92849cb55b15dce920243e0396
Alec Swift <aleceswift@gmail.com>
2023-03-17 13:18:07 -0700

    Add init_state function

---
+++ checkers_ai.py
@@ -0,0 +1,19 @@
+from board import Piece, BoardDict
+
+State = tuple[tuple[complex, int]]
+
+def init_state(board: BoardDict) -> State:
+    state = []
+    for val in board:
+        if isinstance(val, Piece):
+            if val.color == "black":
+                color = 1
+            else:
+                color = 0
+            if val.rank == "king":
+                color += 1
+            state.append((val.pos, color))
+    return tuple(state)
+
+
+
