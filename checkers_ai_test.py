4e2e6c05edfb861af32d92ba104f245ad1536ac0
Alec Swift <aleceswift@gmail.com>
2023-03-17 23:23:25 -0700

    Add valid paths test

--- checkers_ai_test.py
+++ checkers_ai_test.py
@@ -9,11 +9,13 @@ class TestCheckersAi(unittest.TestCase):
         self.board = Board()
         self.initial_board_dict = self.board.board
         self.initial_state = checkers_ai.init_state(self.initial_board_dict)
+        self.borders = checkers_ai.init_borders()
 
     def tearDown(self):
         del self.board
         del self.initial_board_dict
         del self.initial_state
+        del self.borders
 
     def test_init_state_method_with_initial_board(self):
         actual_output = set(self.initial_state)
@@ -54,9 +56,8 @@ class TestCheckersAi(unittest.TestCase):
         self.assertEqual(actual_output, expected)
 
     def test_valid_move_method_with_initial_board_state(self):
-        borders = checkers_ai.init_borders()
-        state = checkers_ai.State(self.initial_state, borders)
-        state.valid_moves(False)
+        state_obj = checkers_ai.State(self.initial_state, self.borders)
+        state_obj.valid_moves(False)
         expected = set(
             [
                 (1, 5j, 1 + 4j),
@@ -68,7 +69,62 @@ class TestCheckersAi(unittest.TestCase):
                 (1, 6 + 5j, 7 + 4j),
             ]
         )
-        actual_output = set(state.paths)
+        actual_output = set(state_obj.paths)
+        self.assertEqual(expected, actual_output)
+
+    def test_capture_move(self):
+        state = checkers_ai.make_move((1, 5j, 1 + 4j), self.initial_state)
+        state = checkers_ai.make_move((2, 3 + 2j, 2 + 3j), state)
+        print(checkers_ai.state_print(state))
+        expected = set([(1, 1 + 4j, 2 + 3j, 3 + 2j)])
+        self.valid_paths_helper(state, expected)
+
+    def test_valid_paths_method_with_double_capture(self):
+        state = checkers_ai.make_move((1, 5j, 1 + 4j), self.initial_state)
+        state = checkers_ai.make_move((2, 5 + 2j, 4 + 3j), state)
+        state = checkers_ai.make_move((1, 6 + 5j, 7 + 4j), state)
+        state = checkers_ai.make_move((2, 6 + 1j, 5 + 2j), state)
+        state = checkers_ai.make_move((1, 1 + 6j, 5j), state)
+        state = checkers_ai.make_move((2, 4 + 3j, 3 + 4j), state)
+        expected = {(1, 2 + 5j, 3 + 4j, 4 + 3j, 5 + 2j, 6 + 1j)}
+        self.valid_paths_helper(state, expected)
+        
+        
+
+    def test_valid_paths_method_with_double_capture_2(self):
+        state = checkers_ai.make_move((1, 5j, 1 + 4j), self.initial_state)
+        state = checkers_ai.make_move((2, 5 + 2j, 4 + 3j), state)
+        state = checkers_ai.make_move((1, 6 + 5j, 7 + 4j), state)
+        state = checkers_ai.make_move((2, 6 + 1j, 5 + 2j), state)
+        state = checkers_ai.make_move((1, 1 + 6j, 5j), state)
+        state = checkers_ai.make_move((2, 5 + 0j, 6 + 1j), state)
+        state = checkers_ai.make_move((1, 7j, 1 + 6j), state)
+        state = checkers_ai.make_move((2, 3 + 2j, 2 + 3j), state)
+        expected = {(1, 1 + 4j, 2 + 3j, 3 + 2j, 4 + 1j, 5 + 0j)}
+        self.valid_paths_helper(state, expected)
+
+    def test_valid_paths_with_king_double_capture(self):
+        """state = checkers_ai.make_move((1, 6 + 5j, 7 + 4j), self.initial_state)
+        state = checkers_ai.make_move((2, 1 + 2j, 2 + 3j), state)
+        state = checkers_ai.make_move((1, 5 + 6j, 6 + 5j), state)
+        state = checkers_ai.make_move((2, 1j, 1 + 2j), state)
+        state = checkers_ai.make_move((1, 4 + 7j, 5 + 6j), state)
+        state = checkers_ai.make_move((2, 2 + 3j, 3 + 4j), state)
+        state = checkers_ai.make_move(
+            (1, 4 + 5j, 3 + 4j, 2 + 3j, 1 + 2j, 1j), state
+        )
+        state = checkers_ai.make_move((2, 2 + 1j, 1 + 2j), state)
+        state = checkers_ai.make_move((1, 5j, 1 + 4j), state)
+        state = checkers_ai.make_move((2, 1 + 0j, 2 + 1j), state)
+        state = checkers_ai.make_move((1, 1j, 1 + 0j), state)  # black piece promoted
+        state = checkers_ai.make_move((2, 3 + 2j, 4 + 3j), state)
+        expected = {(1, 1 + 0j, 2 + 1j, 3 + 2j, 4 + 3j, 5 + 4j)}
+        self.valid_paths_helper(state, expected)"""
+
+    def valid_paths_helper(self, state, expected):
+        state_obj = checkers_ai.State(state, self.borders)
+        state_obj.valid_moves(False)
+        actual_output = state_obj.paths
         self.assertEqual(expected, actual_output)
 
 if __name__ == "__main__":
