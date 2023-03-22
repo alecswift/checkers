0ec2b9e30001ee7334a32f4c1ad59d2416485815
Alec Swift <aleceswift@gmail.com>
2023-03-21 22:53:41 -0700

    7/8 board tests working

--- board_test.py
+++ board_test.py
@@ -9,14 +9,18 @@ class TestBoard(unittest.TestCase):
         self.borders = board.init_borders()
         self.b_piece = board.Piece.BLACK
         self.w_piece = board.Piece.WHITE
+        self.b_king = board.Piece.KING
         self.empty = board.Piece.EMPTY
+        self.move = game.make_move
 
     def tearDown(self):
         del self.initial_state
         del self.borders
         del self.b_piece
         del self.w_piece
+        del self.b_king
         del self.empty
+        del self.move
 
     def test_init_state_method_with_initial_board(self):
         actual_output = set(self.initial_state)
@@ -71,26 +75,26 @@ class TestBoard(unittest.TestCase):
         self.assertEqual(expected, actual_output)
 
     def test_valid_move_method_with_one_valid_capture(self):
-        state = game.make_move((self.b_piece, (5j, 1 + 4j), ()), self.initial_state)
-        state = game.make_move((self.w_piece, (3 + 2j, 2 + 3j), ()), state)
+        state = self.move((self.b_piece, (5j, 1 + 4j), ()), self.initial_state)
+        state = self.move((self.w_piece, (3 + 2j, 2 + 3j), ()), state)
         expected = {(self.b_piece, (1 + 4j, 3 + 2j), (2 + 3j,))}
         self.valid_paths_helper(state, expected)
 
     def test_valid_move_method_with_one_valid_capture_2(self):
-        state = game.make_move((self.b_piece, (2 + 5j, 3 + 4j), ()), self.initial_state)
-        state = game.make_move((self.w_piece, (3 + 2j, 4 + 3j), ()), state)
-        state = game.make_move((self.b_piece, (1 + 6j, 2 + 5j), ()), state)
-        state = game.make_move((self.w_piece, (5 + 2j, 6 + 3j), ()), state)
-        state = game.make_move((self.b_piece, (3 + 4j, 5 + 2j), (4 + 3j,)), state)
+        state = self.move((self.b_piece, (2 + 5j, 3 + 4j), ()), self.initial_state)
+        state = self.move((self.w_piece, (3 + 2j, 4 + 3j), ()), state)
+        state = self.move((self.b_piece, (1 + 6j, 2 + 5j), ()), state)
+        state = self.move((self.w_piece, (5 + 2j, 6 + 3j), ()), state)
+        state = self.move((self.b_piece, (3 + 4j, 5 + 2j), (4 + 3j,)), state)
         expected = {(self.w_piece, (6 + 1j, 4 + 3j), (5 + 2j,))}
         self.valid_paths_helper(state, expected, True)
     
     def test_valid_move_method_with_2_valid_captures(self):
-        state = game.make_move((self.b_piece, (4 + 5j, 5 + 4j), ()), self.initial_state)
-        state = game.make_move((self.w_piece, (5 + 2j, 6 + 3j), ()), state)
-        state = game.make_move((self.b_piece, (5 + 6j, 4 + 5j), ()), state)
-        state = game.make_move((self.w_piece, (3 + 2j, 4 + 3j), ()), state)
-        state = game.make_move((self.b_piece, (5 + 4j, 3 + 2j), (4 + 3j,)), state)
+        state = self.move((self.b_piece, (4 + 5j, 5 + 4j), ()), self.initial_state)
+        state = self.move((self.w_piece, (5 + 2j, 6 + 3j), ()), state)
+        state = self.move((self.b_piece, (5 + 6j, 4 + 5j), ()), state)
+        state = self.move((self.w_piece, (3 + 2j, 4 + 3j), ()), state)
+        state = self.move((self.b_piece, (5 + 4j, 3 + 2j), (4 + 3j,)), state)
         expected = {
                 (self.w_piece, (2 + 1j, 4 + 3j), (3 + 2j,)),
                 (self.w_piece, (4 + 1j, 2 + 3j), (3 + 2j,)),
@@ -98,41 +102,41 @@ class TestBoard(unittest.TestCase):
         self.valid_paths_helper(state, expected, True)
 
     def test_valid_paths_method_with_double_capture(self):
-        state = game.make_move((1, 5j, 1 + 4j), self.initial_state)
-        state = game.make_move((2, 5 + 2j, 4 + 3j), state)
-        state = game.make_move((1, 6 + 5j, 7 + 4j), state)
-        state = game.make_move((2, 6 + 1j, 5 + 2j), state)
-        state = game.make_move((1, 1 + 6j, 5j), state)
-        state = game.make_move((2, 4 + 3j, 3 + 4j), state)
-        expected = {(1, 2 + 5j, 3 + 4j, 4 + 3j, 5 + 2j, 6 + 1j)}
+        state = self.move((self.b_piece, (5j, 1 + 4j), ()), self.initial_state)
+        state = self.move((self.w_piece, (5 + 2j, 4 + 3j), ()), state)
+        state = self.move((self.b_piece, (6 + 5j, 7 + 4j), ()), state)
+        state = self.move((self.w_piece, (6 + 1j, 5 + 2j), ()), state)
+        state = self.move((self.b_piece, (1 + 6j, 5j), ()), state)
+        state = self.move((self.w_piece, (4 + 3j, 3 + 4j), ()), state)
+        expected = {(self.b_piece, (2 + 5j, 4 + 3j, 6 + 1j), (3 + 4j, 5 + 2j))}
         self.valid_paths_helper(state, expected)
 
     def test_valid_paths_method_with_double_capture_2(self):
-        state = game.make_move((1, 5j, 1 + 4j), self.initial_state)
-        state = game.make_move((2, 5 + 2j, 4 + 3j), state)
-        state = game.make_move((1, 6 + 5j, 7 + 4j), state)
-        state = game.make_move((2, 6 + 1j, 5 + 2j), state)
-        state = game.make_move((1, 1 + 6j, 5j), state)
-        state = game.make_move((2, 5 + 0j, 6 + 1j), state)
-        state = game.make_move((1, 7j, 1 + 6j), state)
-        state = game.make_move((2, 3 + 2j, 2 + 3j), state)
-        expected = {(1, 1 + 4j, 2 + 3j, 3 + 2j, 4 + 1j, 5 + 0j)}
+        state = self.move((self.b_piece, (5j, 1 + 4j), ()), self.initial_state)
+        state = self.move((self.w_piece, (5 + 2j, 4 + 3j), ()), state)
+        state = self.move((self.b_piece, (6 + 5j, 7 + 4j), ()), state)
+        state = self.move((self.w_piece, (6 + 1j, 5 + 2j), ()), state)
+        state = self.move((self.b_piece, (1 + 6j, 5j), ()), state)
+        state = self.move((self.w_piece, (5 + 0j, 6 + 1j), ()), state)
+        state = self.move((self.b_piece, (7j, 1 + 6j), ()), state)
+        state = self.move((self.w_piece, (3 + 2j, 2 + 3j), ()), state)
+        expected = {(self.b_piece, (1 + 4j, 3 + 2j, 5 + 0j), (2 + 3j,  4 + 1j))}
         self.valid_paths_helper(state, expected)
 
     def test_valid_paths_with_king_double_capture(self):
-        state = game.make_move((1, 6 + 5j, 7 + 4j), self.initial_state)
-        state = game.make_move((2, 1 + 2j, 2 + 3j), state)
-        state = game.make_move((1, 5 + 6j, 6 + 5j), state)
-        state = game.make_move((2, 1j, 1 + 2j), state)
-        state = game.make_move((1, 4 + 7j, 5 + 6j), state)
-        state = game.make_move((2, 2 + 3j, 3 + 4j), state)
-        state = game.make_move((1, 4 + 5j, 3 + 4j, 2 + 3j, 1 + 2j, 1j), state)
-        state = game.make_move((2, 2 + 1j, 1 + 2j), state)
-        state = game.make_move((1, 5j, 1 + 4j), state)
-        state = game.make_move((2, 1 + 0j, 2 + 1j), state)
-        state = game.make_move((1, 1j, 1 + 0j), state)  # black piece promoted
-        state = game.make_move((2, 3 + 2j, 4 + 3j), state)
-        expected = {(3, 1 + 0j, 2 + 1j, 3 + 2j, 4 + 3j, 5 + 4j)}
+        state = self.move((self.b_piece, (6 + 5j, 7 + 4j), ()), self.initial_state)
+        state = self.move((self.w_piece, (1 + 2j, 2 + 3j), ()), state)
+        state = self.move((self.b_piece, (5 + 6j, 6 + 5j), ()), state)
+        state = self.move((self.w_piece, (1j, 1 + 2j), ()), state)
+        state = self.move((self.b_piece, (4 + 7j, 5 + 6j), ()), state)
+        state = self.move((self.w_piece, (2 + 3j, 3 + 4j), ()), state)
+        state = self.move((self.b_piece, (4 + 5j, 2 + 3j, 1j), ( 3 + 4j, 1 + 2j)), state)
+        state = self.move((self.w_piece, (2 + 1j, 1 + 2j), ()), state)
+        state = self.move((self.b_piece, (5j, 1 + 4j), ()), state)
+        state = self.move((self.w_piece, (1 + 0j, 2 + 1j), ()), state)
+        state = self.move((self.b_piece, (1j, 1 + 0j), ()), state)  # black piece promoted
+        state = self.move((self.w_piece, (3 + 2j, 4 + 3j), ()), state)
+        expected = {(self.b_king, (1 + 0j, 3 + 2j, 5 + 4j), (2 + 1j, 4 + 3j))}
         self.valid_paths_helper(state, expected)
 
     def valid_paths_helper(self, state, expected, max_player=False):
